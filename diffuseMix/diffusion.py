c4bcfbd5b51fea11a514750697dd56334a2cabcb
AustingDong <a7dong@uwaterloo.ca>
2024-09-05 18:49:00 -0400

    u

--- diffuseMix/diffusion.py
+++ diffuseMix/diffusion.py
@@ -12,9 +12,9 @@ class Diffusion:
         
         gen_img = self.pipeline(
             prompt = prompt,
-            negative_prompt="Oversaturated, blurry, low quality, strange eyes, strange hands", # What NOT to generate
+            negative_prompt="lowres, bad anatomy, bad hands, text, error, (missing fingers, extra digit, fewer digits:1.3), cropped, worst quality, low quality, normal quality, jpeg artifacts,signature, watermark, username, blurry, artist name, (worst quality, low quality, extra digits:1.3), detached collar,(juvenile:1.3), (simplistic:1.3), (lack of maturity:1.3), (unconvincing age:1.3), (cliche:1.3), Poor quality image, (low resolution:1.3), (pixelated:1.3), (blurred:1.3), (grainy:1.3), (noisy:1.3), (compression artifacts:1.3), (over-exposure:1.3), (under-exposure:1.3), (bad lighting:1.3), (poor composition:1.3), (unattractive:1.3), (unappealing:1.3), (unedited:1.3), (amateur:1.3),(three arms:1.3)(poorly drawn hands:1.3),",
             image=image,
-            guidance_scale=8,          # How strongly to follow the prompt
+            guidance_scale=8,
             generator = self.generator 
         )
 
--- diffuseMix/utils.py
+++ diffuseMix/utils.py
@@ -1,5 +1,6 @@
 from diffuseMix.diffusion import Diffusion
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 
 class Utils:
@@ -15,24 +16,33 @@ class Utils:
         org_img_arr = np.array(org_img)
         gen_img_arr = np.array(gen_img)
 
+        assert org_img_arr.shape == gen_img_arr.shape
+
         ones_matrix = np.ones(shape=org_img_arr.shape)
         
 
         mat_u1 = np.zeros(shape=org_img_arr.shape)
-        mat_u1[:org_img_arr.shape[0], :] = 1
+        mat_u1[:org_img_arr.shape[0] // 2, :] = 1
         mat_u0 = ones_matrix - mat_u1
 
         mat_l1 = np.zeros(shape=org_img_arr.shape)
-        mat_l1[:, :org_img_arr.shape[1]] = 1
+        mat_l1[:, :org_img_arr.shape[1] // 2] = 1
         mat_l0 = ones_matrix - mat_l1
 
         masks = [mat_u0, mat_u1, mat_l0, mat_l1]
 
-        mask = random(masks)
+        mask = random.choice(masks)
 
-        print(org_img_arr.shape)
-        print(gen_img_arr.shape)
-        print(ones_matrix.shape)
-        print(mask)
+        combined_img = mask * org_img + (ones_matrix - mask) * gen_img
 
-        return mask * org_img + (ones_matrix - mask) * gen_img
+        return combined_img.astype(int)
+
+    @staticmethod
+    def fractal_mixing(combined_img, fractal_img, ld=0.2):
+        combined_img_arr = np.array(combined_img)
+        fractal_img_arr = np.array(fractal_img)
+
+        assert combined_img_arr.shape == fractal_img_arr.shape
+
+        return fractal_img_arr * ld + combined_img_arr * (1 - ld)
+
