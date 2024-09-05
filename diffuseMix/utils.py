430d568c159bd0af9c382c3577df219e483d4473
AustingDong <a7dong@uwaterloo.ca>
2024-09-05 16:54:26 -0400

    Update util.py

--- diffuseMix/utils.py
+++ diffuseMix/utils.py
@@ -1,5 +1,6 @@
 from diffuseMix.diffusion import Diffusion
 import numpy as np
+import random
 
 class Utils:
     
@@ -15,9 +16,23 @@ class Utils:
         gen_img_arr = np.array(gen_img)
 
         ones_matrix = np.ones(shape=org_img_arr.shape)
+        
+
+        mat_u1 = np.zeros(shape=org_img_arr.shape)
+        mat_u1[:org_img_arr.shape[0], :] = 1
+        mat_u0 = ones_matrix - mat_u1
+
+        mat_l1 = np.zeros(shape=org_img_arr.shape)
+        mat_l1[:, :org_img_arr.shape[1]] = 1
+        mat_l0 = ones_matrix - mat_l1
+
+        masks = [mat_u0, mat_u1, mat_l0, mat_l1]
+
+        mask = random(masks)
 
         print(org_img_arr.shape)
         print(gen_img_arr.shape)
         print(ones_matrix.shape)
+        print(mask)
 
-
+        return mask * org_img + (ones_matrix - mask) * gen_img
