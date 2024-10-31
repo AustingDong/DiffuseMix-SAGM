47aeb11ba90e90a598954dff732ee775da2f513b
AustingDong <a7dong@uwaterloo.ca>
2024-10-31 17:56:52 -0400

    Update algorithms.py

--- domainbed/algorithms/algorithms.py
+++ domainbed/algorithms/algorithms.py
@@ -119,7 +119,7 @@ class ERM_DiffuseMix(Algorithm):
     """
 
     def __init__(self, input_shape, num_classes, num_domains, hparams):
-        super(ERM, self).__init__(input_shape, num_classes, num_domains, hparams)
+        super(ERM_DiffuseMix, self).__init__(input_shape, num_classes, num_domains, hparams)
         self.featurizer = networks.Featurizer(input_shape, self.hparams)
         self.classifier = nn.Linear(self.featurizer.n_outputs, num_classes)
         self.network = nn.Sequential(self.featurizer, self.classifier)
