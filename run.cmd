37df6ff9e40dda23ff4c0d8b02b23c6543d3ab85
AustingDong <a7dong@uwaterloo.ca>
2024-11-06 22:56:07 -0500

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-python train_all.py exp_name --dataset PACS --data_dir  --trial_seed 0 --algorithm ERM --checkpoint_freq 100 --alpha 0.001 --lr 3e-5 --weight_decay 1e-4 --resnet_dropout 0.5 --swad False
+python train_all.py exp_name --dataset PACS --data_dir  --trial_seed 0 --algorithm ERM_DiffuseMix --checkpoint_freq 100 --alpha 0.001 --lr 3e-5 --weight_decay 1e-4 --resnet_dropout 0.5 --swad False
