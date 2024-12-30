5f8b31ebb514a9a42985f59c42f785d387fb300a
AustingDong <a7dong@uwaterloo.ca>
2024-12-30 18:10:26 -0500

    Update transforms.py

--- domainbed/datasets/transforms.py
+++ domainbed/datasets/transforms.py
@@ -1,6 +1,5 @@
 from torchvision import transforms as T
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+import torchvision.transforms.v2 as v2
 import torch
 
 def get_basic(x):
@@ -15,16 +14,19 @@ def get_basic(x):
 
 def get_aug(x):
     if isinstance(x, tuple):
-        aug = A.Compose([
-            A.RandomResizedCrop(height=224, width=224, scale=(0.7, 1.0)),
-            A.HorizontalFlip(p=0.5),
-            A.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
-            A.ToGray(p=0.1),
-            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ToTensorV2(),
-        ])
-        aug_imgs = aug(image=x[0].numpy(), mask=x[1].numpy())
-        return (aug_imgs['image'], aug_imgs['mask'])
+        seed = torch.randint(0, 10000, (1,)).item()
+        torch.manual_seed(seed)
+        aug = T.Compose(
+            [
+                T.RandomResizedCrop(224, scale=(0.7, 1.0)),
+                T.RandomHorizontalFlip(),
+                T.ColorJitter(0.3, 0.3, 0.3, 0.3),
+                T.RandomGrayscale(p=0.1),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        return tuple(aug(xx) for xx in x)
     
     else:
         aug = T.Compose(
