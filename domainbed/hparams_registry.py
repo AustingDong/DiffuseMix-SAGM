c88827eeacd90e225ae08b53da99f1f5c9dff66d
AustingDong <a7dong@uwaterloo.ca>
2025-01-04 00:05:31 -0500

    increase batch_size to 64

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -25,9 +25,9 @@ def _hparams(algorithm: str, dataset: str, random_state: np.random.RandomState):
     if dataset not in SMALL_IMAGES:
         hparams["lr"] = (5e-5, 10 ** random_state.uniform(-5, -3.5))
         if dataset == "DomainNet":
-            hparams["batch_size"] = (32, int(2 ** random_state.uniform(3, 5)))
+            hparams["batch_size"] = (64, int(2 ** random_state.uniform(3, 5)))
         else:
-            hparams["batch_size"] = (32, int(2 ** random_state.uniform(3, 5.5)))
+            hparams["batch_size"] = (64, int(2 ** random_state.uniform(3, 5.5)))
         if algorithm == "ARM":
             hparams["batch_size"] = (8, 8)
     else:
